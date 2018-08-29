b938d631f822e58397ab34c80c52f1554450be62
shosseini <hosseini.saeid@gmail.com>
2018-08-29 14:55:47 +0200

    Update ReadpdfController.php

--- src/Controller/ReadpdfController.php
+++ src/Controller/ReadpdfController.php
@@ -7,61 +7,30 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use App\Services\ReadPdf;
+use Psr\Log\LoggerInterface;
 
 /**
  * Import the PDF Parser class
  */
-use Smalot\PdfParser\Parser;
+//use Smalot\PdfParser\Parser;
 
 class ReadpdfController extends Controller
 {
+
+    // ReadPdf with service
     /**
      * @Route("/readpdf", name="readpdf")
      */
-    public function readPdfAction(Request $request)
+    public function readPdfAction(Request $request, ReadPdf $pdfObj, LoggerInterface $logger)
     {
-        // The relative or absolute path to the PDF file
-        $pdfFile = $request->files->get('pdf-file');
-        if (!is_object($pdfFile)) {
+        $returnArray = $pdfObj->getPdfContent($request);
+        if(!$returnArray) {
             return $this->uploadPdfAction('Select een pdf bestand' );
         }
-        $pdfFilePath = $pdfFile->getPathName();
-        $originFileName = $pdfFile->getClientOriginalName();
-        // Create an instance of the PDFParser
-        $PDFParser = new Parser();
-
-        // Create an instance of the PDF with the parseFile method of the parser
-        // this method expects as first argument the path to the PDF file
-        $pdf = $PDFParser->parseFile($pdfFilePath);
-
-        // Retrieve all pages from the pdf file.
-        $pages  = $pdf->getPages();
-
-        // Retrieve the number of pages by counting the array
-        $totalPages = count($pages);
-
-        // Set the current page as the first (a counter)
-        $currentPage = 1;
-
-        // Create an empty variable that will store thefinal text
-        $text = "";
-
-        // Loop over each page to extract the text
-        foreach ($pages as $page) {
-
-            // Add a HTML separator per page e.g Page 1/14
-            $text .= "<h3>Page $currentPage/$totalPages</h3> </br>";
-
-            // Concatenate the text
-            $text .= $page->getText();
-
-            // Increment the page counter
-            $currentPage++;
-        }
-
         return $this->render('readpdf/readpdf.html.twig', [
-            'pdftext' => $text,
-            'originFileName' => $originFileName
+            'pdftext' => $returnArray['pdfText'],
+            'originFileName' => $returnArray['originFileName']
         ]);
 
         // Send the text as response in the controller
